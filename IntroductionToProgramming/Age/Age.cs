7f45ac16743ecbcbd6187ebb1888fde5bd449f4d
Todor Rusinov <todor.radoslavov.rusinov@gmail.com>
2016-10-18 21:57:18 +0300

    Introduction to programming

---
+++ IntroductionToProgramming/Age/Age.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age
+{
+    class Age
+    {
+        static void Main(string[] args)
+        {
+            DateTime birthDay = DateTime.ParseExact(Console.ReadLine(), "MM.dd.yyyy", CultureInfo.InvariantCulture);
+            DateTime now = DateTime.Now;
+            int age = ((now - birthDay).Days / 365);
+            Console.WriteLine(age);
+            Console.WriteLine(age + 10);
+
+        }
+    }
+}
